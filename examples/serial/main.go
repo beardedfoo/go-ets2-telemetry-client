e14f5e4285ce3f20798cbc5e510a12a4f6a40ff8
Cyle Riggs <beardedfoo@gmail.com>
2016-05-13 21:55:34 -0700

    Add serial port driver example
    Add an example program that sends ETS2 data over a serial port.

--- examples/serial/main.go
+++ examples/serial/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io"
@@ -8,8 +9,8 @@ import (
 	"os"
 	"time"
 
-	ets2 "github.com/cyleriggs/go-ets2-telemetry-client"
-	serial "github.com/tarm/serial"
+	ets2		"github.com/cyleriggs/go-ets2-telemetry-client"
+	serial 	"github.com/goburrow/serial"
 )
 
 var baseUrl = ""
@@ -18,8 +19,8 @@ var serialPort = ""
 
 func init() {
 	flag.StringVar(&baseUrl, "baseUrl", "http://localhost:25555", "HTTP url for telemetry server")
-	flag.IntVar(&updateFreq, "updateFreq", 1000/30, "Update frequency in milliseconds")
-	flag.StringVar(&serialPort, "serialPort", "", "A serial port to write updates to, one var at a time")
+	flag.IntVar(&updateFreq, "updateFreq", 1000/24, "Update frequency in milliseconds")
+	flag.StringVar(&serialPort, "serialPort", "COM3", "A serial port to write updates to, one var at a time")
 }
 
 func main() {
@@ -28,9 +29,11 @@ func main() {
 	var err error
 
 	// Open serial port (optional)
-	var fSerial io.ReadWriteCloser
 	fmt.Printf("Connecting to serial...")
-	fSerial, err = serial.OpenPort(&serial.Config{Name: serialPort, Baud: 115200})
+	serialConfig := &serial.Config{
+		Address: serialPort, BaudRate: 115200, StopBits: 1,
+		Timeout: 100 * time.Millisecond, Parity: "N"}
+	fSerial, err := serial.Open(serialConfig)
 	if err != nil {
 		log.Fatalf("Failed to open serial port: %v", err)
 		os.Exit(1)
@@ -42,14 +45,6 @@ func main() {
 		log.Fatalf("Error writing to serial: %v", err)
 		os.Exit(-1)
 	}
-
-	log.Printf("reading from serial")
-	in := make([]byte, 64)
-	if _, err := fSerial.Read(in); err != nil {
-		log.Printf("serial read error: %v", err)
-		os.Exit(-1)
-	}
-	log.Printf("serial: %v", in)
 
 	monitor(fSerial, "\n", "")
 }
@@ -64,26 +59,24 @@ func monitor(fh io.ReadWriteCloser, valueSep string, groupSep string) {
 			log.Fatalf("Error reading telemetry data: %v", err)
 			os.Exit(-1)
 		}
-		log.Printf("sending cmds")
-
 		sendCmd(fh, fmt.Sprintf("rpm=%f%s", t.Truck.EngineRpm, valueSep))
 		sendCmd(fh, fmt.Sprintf("kmh=%f%s", t.Truck.Speed, valueSep))
+		sendCmd(fh, fmt.Sprintf("fuel=%f%s", t.Truck.FuelCapacity/t.Truck.Fuel, valueSep))
 
 		time.Sleep(time.Duration(updateFreq * int(time.Millisecond)))
 
-		log.Printf("reading from serial")
-		in := make([]byte, 64)
-		if _, err := fh.Read(in); err != nil {
-			log.Printf("serial read error: %v", err)
-			os.Exit(-1)
-		}
-		log.Printf("serial: %v", in)
 	}
 }
 
-func sendCmd(fh io.Writer, cmd string) {
+func sendCmd(fh io.ReadWriter, cmd string) {
 	if _, err := io.WriteString(fh, cmd); err != nil {
 		log.Fatalf("Failed to write cmd: %v", err)
 		os.Exit(-1)
+	} else {
+		scanner := bufio.NewScanner(fh)
+		scanner.Scan()
+		log.Printf("serial: %v", scanner.Text())
+		scanner.Scan()
+		log.Printf("serial: %v", scanner.Text())
 	}
 }
